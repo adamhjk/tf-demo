286a05e525bd198cc276abad944adb056b151043
Adam Jacob <adam@systeminit.com>
2024-09-02 10:14:27 -0700

    working vpc

--- main.tf
+++ main.tf
@@ -1,11 +1,16 @@
 terraform {
+  cloud {
+    organization = "demo_time"
+    workspaces {
+        name = "demo_time"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.16"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
